aad840569f8ae60008da6fb591eef48e57b76bdf
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-07-25 10:42:03 -0600

    basic layout

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,19 +2,24 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { SocialIcon } from 'react-social-icons'
 import { List, ListItem } from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 export default function SocialLinks() {
     const name = useSelector((state: RootState) => state.ui.name);
     const social = useSelector((state: RootState) => state.ui.social);
 
+    const iconClass = (key: string) => key !== 'opentable' ? 'fa-brands fa-' + key : 'fa-solid fa-circle-dot'
+
     return (
-        <List>
+        <ul className="list-inline">
             {Object.keys(social).map((key) => (
-                <ListItem key={key}>
-                    <SocialIcon url={social[key]} title={`${name} ${key}`}></SocialIcon>
-                </ListItem>
+                <li className="list-inline-item" key={key}>
+                    <Link to={social[key]} title={`${name} ${key}`}>
+                        <i className={iconClass(key)}></i>
+                    </Link>
+                </li>
             ))}
-        </List>
+        </ul>
     )
 };
