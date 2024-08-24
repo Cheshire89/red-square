66a1e0eb52acdd5308ec9068ac4e2d09e2c2c164
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-08-24 13:44:38 -0600

    food menu item styling

--- src/components/MenuItems/FoodMenuItem.tsx
+++ src/components/MenuItems/FoodMenuItem.tsx
@@ -1,12 +1,15 @@
 import { MenuItem } from "@models/MenuItem.model";
+import styles from "./MenuItems.module.scss";
 
 export default function FoodMenuItem({ item }: { item: MenuItem }) {
-    const { itemTitle, itemDesc, itemPrice }: MenuItem = item;
-    return (<li>
-        <strong>{itemTitle}</strong>
-        <p>
-            <span>{itemDesc}</span>
-            <span>${itemPrice}</span>
-        </p>
-    </li>)
+  const { itemTitle, itemDesc, itemPrice }: MenuItem = item;
+  return (
+    <li className={styles.foodItem}>
+      <strong>{itemTitle}</strong>
+      <p>
+        <span>{itemDesc}</span>
+        <span>${itemPrice}</span>
+      </p>
+    </li>
+  );
 }
