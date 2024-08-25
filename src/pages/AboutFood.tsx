492c7606a44b27616533aa37b6d03b6ebc4ba63a
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-08-24 18:34:52 -0600

    about sections content

--- src/pages/AboutFood.tsx
+++ src/pages/AboutFood.tsx
@@ -1,30 +1,42 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { ContentBlock } from "../components/ContentBlock/ContentBlock";
+import PhoneNum from "../components/PhoneNum/PhoneNum";
 
 export default function AboutFood() {
-    return (
-        <>
-            <Container>
-                <Row>
-                    <Col md={6}>
-                        <h1 className="header text-uppercase">BISTRO + VODKA BAR</h1>
-                        <p>
-                            Known for the largest selection of vodkas in Denver, Red Square
-                            couples a fun and social bar scene with a European-inspired dining
-                            experience that blends new and old flavors in a vibrant and romantic
-                            atmosphere. Red Square European Bistro opened in 2003 in the vibrant
-                            open-air, mixed-use shopping destination, Writer Square. Writer Square
-                            is just off 16th Street Mall and is in close proximity to Larimer Square
-                            and the Theater District, making it a perfect destination for a night out.
-                            Irina, Max and the entire staff are always happy to see you and treat
-                            you as a part of the family.
-                        </p>
-                    </Col>
-                    <Col md={6}>
-                        <img className="img-fluid" src="" alt="Dummy" />
-                    </Col>
-
-                </Row>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <ContentBlock background="grey">
+        <Container>
+          <Row>
+            <Col md={6} className="page-text">
+              <h3>Our Food</h3>
+              <p>
+                Our menu changes seasonally, aligning with local and fresh
+                ingredients whenever possible. The food evokes the nostalgic
+                comfort and familiarity of old world European flavors with an
+                updated twist that is playful and refined.
+              </p>
+              <p>
+                Chef Max Ionikh chooses every ingredient, carefully building
+                each dish around just a few key ingredients that come together
+                to build an exquisite flavor profile.
+              </p>
+              <p>
+                Our kitchen makes everything from basic and raw. Every element
+                of your meal, from the sauces all the way through to the
+                desserts, are made from scratch daily.
+              </p>
+              <p>
+                Our chef can modify for almost any dietary restriction; please
+                call ahead at <PhoneNum />.
+              </p>
+            </Col>
+            <Col md={6}>
+              <img className="img-fluid" src="" alt="Dummy" />
+            </Col>
+          </Row>
+        </Container>
+      </ContentBlock>
+    </>
+  );
 }
