c87cf39f936b045fcad09c58c933ee0a79e14de3
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-08-24 18:16:32 -0600

    finish menu styling

--- src/components/MenuItems/VodkaMenuItem.tsx
+++ src/components/MenuItems/VodkaMenuItem.tsx
@@ -1,12 +1,17 @@
 import { VodkaItem } from "@models/MenuItem.model";
+import styles from "./MenuItems.module.scss";
 
 export default function VodkaMenuItem({ item }: { item: VodkaItem }) {
-    const { vodkaTitle, priceShot, priceCarafe } = item;
-    return (<li>
-        <p>
-            <span>{vodkaTitle}</span>
-            <span>${priceShot}</span>
-            <span>${priceCarafe}</span>
-        </p>
-    </li>)
+  const { vodkaTitle, priceShot, priceCarafe } = item;
+  return (
+    <li className={styles.vodkaItem}>
+      <p>
+        <span>{vodkaTitle}</span>
+        <div className={styles.vodkaItemPrice}>
+          <span>Shot: ${priceShot}</span>
+          <span>Carafe: ${priceCarafe}</span>
+        </div>
+      </p>
+    </li>
+  );
 }
