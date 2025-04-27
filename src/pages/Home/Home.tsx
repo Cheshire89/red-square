add65190c821d70d612523b03aa1fad17e30749f
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2025-04-27 14:28:08 -0600

    minimize images fix carousel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,25 @@
 import Carousel from "react-bootstrap/Carousel";
 import { Container, Row, Col } from "react-bootstrap";
+
 import InfusedVodkaImg from "@assets/infused_vodkas.jpg";
 import RibsImg from "@assets/ribs.jpg";
 import VodkaPourImg from "@assets/vodkapour.jpg";
 import RestaurantImg from "@assets/restaurant.jpg";
+
 import "./Home.scss";
 import { ContentBlock } from "../../components/ContentBlock/ContentBlock";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { debounce } from "lodash";
 import { useDispatch, useSelector } from "react-redux";
 import { getOpenTableId } from "@profileStore/profile.slice";
+
 import {
   getContent,
   getContentStatus,
   getPageContent,
   setContent,
 } from "@contentStore/content.slice";
+
 import ReactHtmlParser from "react-html-parser";
 
 const widgetUrl = (
@@ -27,11 +31,19 @@ const widgetUrl = (
 
 let widgetContainer: HTMLElement | null = null;
 
+const isWhitsEnd = process.env.REACT_APP_ID === "06yv1kzt9c7qcpp";
+const images = [
+  { src: RibsImg, alt: "Ribs" },
+  { src: InfusedVodkaImg, alt: "Infused Vodka" },
+  { src: RestaurantImg, alt: "Restaurant Dining Area" },
+  { src: VodkaPourImg, alt: "Vodka Pour" },
+];
+
 export default function Home() {
-  const content = useSelector(getContent);
-  const contentStatus = useSelector(getContentStatus);
   const dispatch = useDispatch<any>();
-  const openTableId = useSelector(getOpenTableId);
+  const content = useSelector(useMemo(() => getContent, []));
+  const contentStatus = useSelector(useMemo(() => getContentStatus, []));
+  const openTableId = useSelector(useMemo(() => getOpenTableId, []));
 
   const appendWidget = debounce(() => {
     let winWidth = window.innerWidth;
@@ -85,26 +97,27 @@ export default function Home() {
     }
   }
 
+  function displayCarousel() {
+    const imagesToShow = isWhitsEnd ? images.slice(0, 1) : images;
+    return (
+      <Carousel
+        className="home-carousel"
+        controls={!isWhitsEnd}
+        indicators={!isWhitsEnd}
+      >
+        {imagesToShow.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img src={image.src} alt={image.alt} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    );
+  }
+
   return (
     <>
-      <Carousel>
-        <Carousel.Item>
-          <img src={RibsImg} alt="Ribs" />
-        </Carousel.Item>
-        {process.env.REACT_APP_ID === "eilv81c1e648nhv" && (
-          <>
-            <Carousel.Item>
-              <img src={InfusedVodkaImg} alt="Infused Vodka" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src={RestaurantImg} alt="Restaurant Dining Area" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src={VodkaPourImg} alt="Vodka Pour" />
-            </Carousel.Item>
-          </>
-        )}
-      </Carousel>
+      {displayCarousel()}
+
       {content && (
         <>
           <ContentBlock background="grey">
